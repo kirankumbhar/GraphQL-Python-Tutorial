2944c4df7a3c633b0b9335cb6cd4e296cd8cee2a
Kiran Kumbhar <kiranpk189@gmail.com>
2020-10-27 12:49:28 +0530

    add voting

--- links/schema.py
+++ links/schema.py
@@ -1,8 +1,10 @@
 import graphene
-
+from graphql import GraphQLError
 from graphene_django import DjangoObjectType
 
-from .models import Link
+from django.db.models import Q
+
+from .models import Link, Vote
 from users.schema import UserType
 
 
@@ -11,11 +13,24 @@ class LinkType(DjangoObjectType):
         model = Link
 
 
-class Query(graphene.ObjectType):
-    links = graphene.List(LinkType)
+class VoteType(DjangoObjectType):
+    class Meta:
+        model = Vote
 
-    def resolve_links(self, info, **kwargs):
+
+class Query(graphene.ObjectType):
+    links = graphene.List(LinkType, search=graphene.String())
+    votes = graphene.List(VoteType)
+
+    def resolve_links(self, info, search=None, **kwargs):
+        if search:
+            return Link.objects.filter(
+                Q(url__icontains=search) | Q(description__icontains=search)
+            )
         return Link.objects.all()
+
+    def resolve_votes(self, info, **kwargs):
+        return Vote.objects.all()
 
 
 class CreateLink(graphene.Mutation):
@@ -40,5 +55,30 @@ class CreateLink(graphene.Mutation):
         )
 
 
+class CreateVote(graphene.Mutation):
+    user = graphene.Field(UserType)
+    link = graphene.Field(LinkType)
+
+    class Arguments:
+        link_id = graphene.Int()
+
+    def mutate(self, info, link_id):
+        user = info.context.user
+        if user.is_anonymous:
+            raise GraphQLError("You must log in to vote")
+
+        link = Link.objects.filter(id=link_id).first()
+        if not link:
+            raise Exception("link does not exists")
+
+        Vote.objects.create(
+            user=user,
+            link=link,
+        )
+
+        return CreateVote(user=user, link=link)
+
+
 class Mutation(graphene.ObjectType):
     create_link = CreateLink.Field()
+    create_vote = CreateVote.Field()
